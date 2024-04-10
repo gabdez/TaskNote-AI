f9e63fddda333d823404ebef135a46c0b7332135
FERNANDEZ CASA Gabriel 00050553 <gabriel.fernandezcasa@3ds.com>
2024-04-10 14:54:32 +0200

    update background cover in lg screen

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-container fluid class="bg-container bg-veryDarkBlue h-100 pt-15 d-flex justify-center align-start">
+    <v-container fluid class="bg-container bg-contain bg-veryDarkBlue h-100 pt-15 d-flex justify-center align-start">
       <TodoContainer />
     </v-container>
   </v-app>
@@ -9,14 +9,11 @@
 <style scoped>
 .bg-container {
   background-image: url("./assets/images/bg-desktop-dark.jpg");
-  background-size: contain;
+  background-size: auto;
 }
 </style>
 
 <script setup lang="ts">
-import { useAppStore } from "@/stores/app";
 import TodoContainer from "@/components/todo-container.vue"
-
-const appStore = useAppStore();
 
 </script>
