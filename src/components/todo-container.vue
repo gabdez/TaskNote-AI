173d983af6b0509efff41a059fb5522c3c5aabe3
FERNANDEZ CASA Gabriel 00050553 <gabriel.fernandezcasa@3ds.com>
2024-04-10 20:02:45 +0200

    add draggable items to reorder list

--- src/components/todo-container.vue
+++ src/components/todo-container.vue
@@ -3,11 +3,17 @@
         <TodoContainerHeader />
         <TodoNewLine />
         <v-sheet class="w-100" elevation="10" rounded>
-            <TodoLine v-for="(todo, index) in appStore.filteredTodos" :key="todo.id" :todo
-                :roundedCorner="index === 0" />
+            <VueDraggableNext class="dragArea list-group w-full" :list="appStore.filteredTodos" @change="log">
+                <TodoLine v-for="(todo, index) in appStore.filteredTodos" :key="todo.id" :todo
+                    :roundedCorner="index === 0" />
+            </VueDraggableNext>
             <TodoContainerFooter />
         </v-sheet>
+        <!-- Display footer for mobile screen only -->
         <TodoContainerFooterMobile />
+        <div v-if="appStore.toggleActive === 'all'" class="mt-10 text-body-1 text-darkGrayishBlue">
+            Drag and Drop to reorder list items
+        </div>
     </v-sheet>
 </template>
 
@@ -19,8 +25,14 @@ import TodoContainerFooterMobile from '@/components/todo-container-footer-mobile
 import TodoContainerHeader from '@/components/todo-container-header.vue';
 import TodoLine from '@/components/todo-line.vue';
 import TodoNewLine from '@/components/todo-new-line.vue';
+import { VueDraggableNext } from 'vue-draggable-next'
 import { useAppStore } from '@/stores/app';
 
 const appStore = useAppStore();
+
+
+const log = (event: any) => {
+    console.log(event)
+};
 
 </script>
