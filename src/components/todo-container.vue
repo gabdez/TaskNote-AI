dad13524e1668edc08d350f847373f9dcf33c27c
FERNANDEZ CASA Gabriel 00050553 <gabriel.fernandezcasa@3ds.com>
2024-04-11 00:37:11 +0200

    add todo list max height

--- src/components/todo-container.vue
+++ src/components/todo-container.vue
@@ -2,20 +2,26 @@
     <v-sheet fluid max-width="750px" class="bg-transparent w-100 d-flex flex-column align-center">
         <TodoContainerHeader />
         <TodoNewLine />
-        <v-sheet class="w-100" elevation="10" rounded>
+        <v-sheet class="w-100 todo-scrollable" elevation="10">
             <VueDraggableNext class="dragArea list-group w-full" :list="appStore.filteredTodos" @change="log">
                 <TodoLine v-for="(todo, index) in appStore.filteredTodos" :key="todo.id" :todo
                     :roundedCorner="index === 0" />
             </VueDraggableNext>
-            <TodoContainerFooter />
         </v-sheet>
+        <TodoContainerFooter />
         <!-- Display footer for mobile screen only -->
         <TodoContainerFooterMobile />
         <DragDropMessage />
     </v-sheet>
 </template>
 
-<style scoped></style>
+<style scoped>
+.todo-scrollable {
+    max-height: 475px;
+    overflow-y: scroll;
+    overflow-x: hidden;
+}
+</style>
 
 <script setup lang="ts">
 import TodoContainerFooter from '@/components/todo-container-footer.vue';
