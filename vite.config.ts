c45c484219cd8edbe07335529a3328c6d0aba0cc
FERNANDEZ CASA Gabriel 00050553 <gabriel.fernandezcasa@3ds.com>
2024-04-13 16:23:25 +0200

    update vite base config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/UI-Challenge-frontendmentor-todo-app/",
+  base: "/",
   plugins: [
     vue(),
   ],
